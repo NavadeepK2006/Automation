ac5ae3c8bf2fd658181a8ac577f2b6d419db9901
NavadeepK2006 <navadeepk2006@gmail.com>
2025-07-20 00:19:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
-GEMINI_API_KEY="AIzaSyBdCsWLofFGyu8Z_sOG5IfOR-NPO97-3xU"
+GEMINI_API_KEY="Please give your own key sir"
 WIKISOURCE_URL="https://en.wikisource.org/wiki/The_Gates_of_Morning/Book_1/Chapter_1"
 SCREENSHOT_PATH="screenshots/chapter1.png"
 CHROMA_COLLECTION="book_versions"
+
+
+#Note:- If I give my key here it will delete my key so I'm not giving
